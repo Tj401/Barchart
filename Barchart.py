78ca455e8f2801e8ea14fa809bf9968104ecafcb
TJ <46988445+Tj401@users.noreply.github.com>
2019-01-24 20:29:54 -0500

    Add files via upload

--- Barchart.py
+++ Barchart.py
@@ -4,20 +4,21 @@ Created on Tue Jan 22 01:34:16 2019
 
 @author: kdandebo
 """
-
+#Importing libraries
 import pandas as pd
 import matplotlib.pyplot as plt
 #import seaborn as sns
 
-
+#Importing data file
 df = pd.read_csv("C:/Users/kdandebo/Desktop/Models/Python excercise/mn-budget-detail-2014.csv")
 #print(df.head(5))
 
-#print(df.info())
-#print(df.describe())
+#checking the info of the data
 print(df.columns)
 #sns.pairplot(df)
 #sns.distplot(df['amount'])
+
+# Applying the condition accoridng to our crtieria
 y = df[df['amount'] >= 4500000]
 #y = df[df['amount']]
 plt.xlabel('University name')
